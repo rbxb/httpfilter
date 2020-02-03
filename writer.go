5cfbb8a1222b78118ee1cab8115813d79753946e
ryan <26235793+rbxb@users.noreply.github.com>
2020-02-03 14:26:46 -0600

    fixed write calls not updating writer state

--- writer.go
+++ writer.go
@@ -9,16 +9,23 @@ type writerWrapper struct {
 	ok chan bool
 }
 
-func(wrapper * writerWrapper) WriteHeader(statusCode int) {
-	<- wrapper.ok
+func (wrapper *writerWrapper) WriteHeader(statusCode int) {
+	<-wrapper.ok
 	wrapper.ResponseWriter.WriteHeader(statusCode)
 	wrapper.ok <- false
 }
 
-func wrapWriter(w http.ResponseWriter) * writerWrapper {
+func (wrapper *writerWrapper) Write(b []byte) (int, error) {
+	<-wrapper.ok
+	n, err := wrapper.ResponseWriter.Write(b)
+	wrapper.ok <- false
+	return n, err
+}
+
+func wrapWriter(w http.ResponseWriter) *writerWrapper {
 	wrapper := &writerWrapper{
 		ResponseWriter: w,
-		ok: make(chan bool, 1),
+		ok:             make(chan bool, 1),
 	}
 	wrapper.ok <- true
 	return wrapper
