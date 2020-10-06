556072a3a398471b6d76858457163368b9a40429
ryan <26235793+rbxb@users.noreply.github.com>
2020-10-06 14:51:58 -0500

    http redirect to https

--- cmd/httpfilter/main.go
+++ cmd/httpfilter/main.go
@@ -19,7 +19,7 @@ func init() {
 	flag.StringVar(&port, "port", ":8080", "The address and port the fileserver listens at.")
 	flag.StringVar(&root, "root", "./root", "The directory serving files.")
 	flag.StringVar(&filter, "filter", "", "The filter file to use (optional).")
-	flag.BoolVar(&ssl, "ssl", false, "Use SSL?")
+	flag.BoolVar(&ssl, "ssl", false, "Use SSL? Using SSL will also listen on port 80 to redirect HTTP traffic to HTTPS.")
 	flag.StringVar(&cert, "cert", "./cert.pem", "The path of the SSL certificate.")
 	flag.StringVar(&key, "key", "./key.pem", "The path of the private key.")
 }
@@ -28,8 +28,15 @@ func main() {
 	flag.Parse()
 	sv := httpfilter.NewServer(root, filter)
 	if ssl {
+		go func() {
+			log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)))
+		}()
 		log.Fatal(http.ListenAndServeTLS(port, cert, key, sv))
 	} else {
 		log.Fatal(http.ListenAndServe(port, sv))
 	}
+}
+
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
 }
