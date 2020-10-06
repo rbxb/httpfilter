11ce3f985bd4a8da96fec3e7a86fdef7f9c9ca27
ryan <26235793+rbxb@users.noreply.github.com>
2020-10-06 15:48:55 -0500

    Update writer.go

--- writer.go
+++ writer.go
@@ -1,13 +1,12 @@
 package httpfilter
 
 import (
-	"bufio"
-	"net"
 	"net/http"
 )
 
 type writerWrapper struct {
 	http.ResponseWriter
+	http.Hijacker
 	ok chan byte
 }
 
@@ -29,12 +28,9 @@ func (wr *writerWrapper) Write(b []byte) (int, error) {
 func wrapWriter(w http.ResponseWriter) *writerWrapper {
 	wr := &writerWrapper{
 		ResponseWriter: w,
+		Hijacker:       w.(http.Hijacker),
 		ok:             make(chan byte, 1),
 	}
 	wr.ok <- 0
 	return wr
-}
-
-func (wr *writerWrapper) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	return wr.ResponseWriter.(http.Hijacker).Hijack()
 }
