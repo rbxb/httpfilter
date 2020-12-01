d0702af823a9902f31ce64ebc3aae9c855e2265a
ryan <26235793+rbxb@users.noreply.github.com>
2020-12-01 07:05:54 -0700

    added flag for log file

--- cmd/httpfilter/main.go
+++ cmd/httpfilter/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/rbxb/httpfilter"
 )
@@ -12,6 +13,7 @@ import (
 var port string
 var root string
 var filter string
+var logPath string
 var ssl bool
 var cert string
 var key string
@@ -20,6 +22,7 @@ func init() {
 	flag.StringVar(&port, "port", ":8080", "The address and port the fileserver listens at.")
 	flag.StringVar(&root, "root", "./root", "The directory serving files.")
 	flag.StringVar(&filter, "filter", "", "The filter file to use (optional).")
+	flag.StringVar(&logPath, "log", "", "The log file to write to.")
 	flag.BoolVar(&ssl, "ssl", false, "Use SSL? Using SSL will also listen on port 80 to redirect HTTP traffic to HTTPS.")
 	flag.StringVar(&cert, "cert", "./cert.pem", "The path of the SSL certificate.")
 	flag.StringVar(&key, "key", "./key.pem", "The path of the private key.")
@@ -27,6 +30,14 @@ func init() {
 
 func main() {
 	flag.Parse()
+	if logPath != "" {
+		f, err := os.OpenFile("place.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		log.SetOutput(f)
+	}
 	fs := httpfilter.NewServer(root, filter)
 	server := http.Server{
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)), //disable HTTP/2
